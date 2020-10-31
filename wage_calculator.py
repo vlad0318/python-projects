cf6d314663c9922371fa89a9c76461896d635ad7
vlad0318 <64244622+vlad0318@users.noreply.github.com>
2020-10-30 19:42:48 -0700

    Update wage_calculator.py

--- wage_calculator.py
+++ wage_calculator.py
@@ -1,5 +1,5 @@
 year = 365
-work_days = float (input('how many days do you work: '))
+work_days = float (input('how many days a week do you work: '))
 work_days_fraction = work_days/7
 hours = float (input ('how many hours do you work a day: '))
 wage = float (input('what is your wage: '))
