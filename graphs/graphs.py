38a362e567131d68b21a14f5bbfe4478a1165ed6
vlad0318 <64244622+vlad0318@users.noreply.github.com>
2020-11-18 16:59:04 -0800

    i will try again

---
+++ graphs/graphs.py
@@ -0,0 +1,494 @@
+import turtle
+#start of positive x vairables vairables
+x0=0
+x1=3.5
+x2=x1+3.5
+x3=x2+3.5
+x4=x3+3.5
+x5=17.5
+x6=x5+3.5
+x7=x6+3.5
+x8=x7+3.5
+x9=x8+3.5
+x10=35
+x11=x10+3.5
+x12=x11+3.5
+x13=x12+3.5
+x14=x13+3.5
+x15=52.5
+x20=70
+x25=87.5
+x30=105
+x35=122.5
+x40=140
+x45=157.5
+x50=175
+x55=192.5
+x60=210
+x65=227.5
+x70=245
+x75=262.5
+x80=280
+x85=297.5
+x90=315
+x95=332.5
+x100=350
+#start of negative x vairables vairables
+nx5=-17.5
+nx10=-35
+nx15=-52.5
+nx20=-70
+nx25=-87.5
+nx30=-105
+nx35=-122.5
+nx40=-140
+nx45=-157.5
+nx50=-175
+nx55=-192.5
+nx60=-210
+nx65=-227.5
+nx70=-245
+nx75=-262.5
+nx80=-280
+nx85=-297.5
+nx90=-315
+nx95=-332.5
+nx100=-350
+#start of positive y vairables vairables
+y0=0
+y5=17.5
+y10=35
+y15=52.5
+y20=70
+y25=87.5
+y30=105
+y35=122.5
+y40=140
+y45=157.5
+y50=175
+y55=192.5
+y60=210
+y65=227.5
+y70=245
+y75=262.5
+y80=280
+y85=297.5
+y90=315
+y95=332.5
+y100=350
+#start of negative y vairables vairables
+ny5=-17.5
+ny10=-35
+ny15=-52.5
+ny20=-70
+ny25=-87.5
+ny30=-105
+ny35=-122.5
+ny40=-140
+ny45=-157.5
+ny50=-175
+ny55=-192.5
+ny60=-210
+ny65=-227.5
+ny70=-245
+ny75=-262.5
+ny80=-280
+ny85=-297.5
+ny90=-315
+ny95=-332.5
+ny100=-350
+#setup
+turtle.setup(800,800)
+turtle.hideturtle()
+turtle.speed(1000)
+#start of positive y graph
+turtle.goto(x0,y0)
+turtle.dot()
+turtle.goto(0,350)
+turtle.goto(x0,y5)
+turtle.dot()
+turtle.penup()
+turtle.goto(10,y5-7.5)
+turtle.write("5")
+turtle.goto(x0,y10)
+turtle.dot()
+turtle.goto(10,y10-7.5)
+turtle.write("10")
+turtle.goto(x0,y15)
+turtle.dot()
+turtle.goto(10,y15-7.5)
+turtle.write("15")
+turtle.goto(x0,y20)
+turtle.dot()
+turtle.goto(10,x20-7.5)
+turtle.write("20")
+turtle.goto(x0,y25)
+turtle.dot()
+turtle.goto(10,x25-7.5)
+turtle.write("25")
+turtle.goto(x0,y30)
+turtle.dot()
+turtle.goto(10,x30-7.5)
+turtle.write("30")
+turtle.goto(x0,y35)
+turtle.dot()
+turtle.goto(10,x35-7.5)
+turtle.write("35")
+turtle.goto(x0,y40)
+turtle.dot()
+turtle.goto(10,x40-7.5)
+turtle.write("40")
+turtle.goto(x0,y45)
+turtle.dot()
+turtle.goto(10,x45-7.5)
+turtle.write("45")
+turtle.goto(x0,y50)
+turtle.dot()
+turtle.goto(10,x50-7.5)
+turtle.write("50")
+turtle.goto(x0,y55)
+turtle.dot()
+turtle.goto(10,x55-7.5)
+turtle.write("55")
+turtle.goto(x0,y60)
+turtle.dot()
+turtle.goto(10,x60-7.5)
+turtle.write("60")
+turtle.goto(x0,y65)
+turtle.dot()
+turtle.goto(10,x65-7.5)
+turtle.write("65")
+turtle.goto(x0,y70)
+turtle.dot()
+turtle.goto(10,x70-7.5)
+turtle.write("70")
+turtle.goto(x0,y75)
+turtle.dot()
+turtle.goto(10,x75-7.5)
+turtle.write("75")
+turtle.goto(x0,y80)
+turtle.dot()
+turtle.goto(10,x80-7.5)
+turtle.write("80")
+turtle.goto(x0,y85)
+turtle.dot()
+turtle.goto(10,x85-7.5)
+turtle.write("85")
+turtle.goto(x0,y90)
+turtle.dot()
+turtle.goto(10,x90-7.5)
+turtle.write("90")
+turtle.goto(x0,y95)
+turtle.dot()
+turtle.goto(10,x95-7.5)
+turtle.write("95")
+turtle.goto(x0,y100)
+turtle.dot()
+turtle.goto(10,x100-7.5)
+turtle.write("100")
+turtle.goto(0,0)
+turtle.pendown()
+
+#start of negative y graph
+turtle.goto(0,-350)
+turtle.penup()
+turtle.goto(x0,ny5)
+turtle.dot()
+turtle.goto(10,ny5-7.5)
+turtle.write("-5")
+turtle.goto(x0,ny10)
+turtle.dot()
+turtle.goto(10,ny10-7.5)
+turtle.write("-10")
+turtle.goto(x0,ny15)
+turtle.dot()
+turtle.goto(10,ny15-7.5)
+turtle.write("-15")
+turtle.goto(x0,ny20)
+turtle.dot()
+turtle.goto(10,ny20-7.5)
+turtle.write("-20")
+turtle.goto(x0,ny25)
+turtle.dot()
+turtle.goto(10,ny25-7.5)
+turtle.write("-25")
+turtle.goto(x0,ny30)
+turtle.dot()
+turtle.goto(10,ny30-7.5)
+turtle.write("-30")
+turtle.goto(x0,ny35)
+turtle.dot()
+turtle.goto(10,ny35-7.5)
+turtle.write("-35")
+turtle.goto(x0,ny40)
+turtle.dot()
+turtle.goto(10,ny40-7.5)
+turtle.write("-40")
+turtle.goto(x0,ny45)
+turtle.dot()
+turtle.goto(10,ny45-7.5)
+turtle.write("-45")
+turtle.goto(x0,ny50)
+turtle.dot()
+turtle.goto(10,ny50-7.5)
+turtle.write("-50")
+turtle.goto(x0,ny55)
+turtle.dot()
+turtle.goto(10,ny55-7.5)
+turtle.write("-55")
+turtle.goto(x0,ny60)
+turtle.dot()
+turtle.goto(10,ny60-7.5)
+turtle.write("-60")
+turtle.goto(x0,ny65)
+turtle.dot()
+turtle.goto(10,ny65-7.5)
+turtle.write("-65")
+turtle.goto(x0,ny70)
+turtle.dot()
+turtle.goto(10,ny70-7.5)
+turtle.write("-70")
+turtle.goto(x0,ny75)
+turtle.dot()
+turtle.goto(10,ny75-7.5)
+turtle.write("-75")
+turtle.goto(x0,ny80)
+turtle.dot()
+turtle.goto(10,ny80-7.5)
+turtle.write("-80")
+turtle.goto(x0,ny85)
+turtle.dot()
+turtle.goto(10,ny85-7.5)
+turtle.write("-85")
+turtle.goto(x0,ny90)
+turtle.dot()
+turtle.goto(10,ny90-7.5)
+turtle.write("-90")
+turtle.goto(x0,ny95)
+turtle.dot()
+turtle.goto(10,ny95-7.5)
+turtle.write("-95")
+turtle.goto(x0,ny100)
+turtle.dot()
+turtle.goto(10,ny100-7.5)
+turtle.write("-100")
+
+#start of positive x 
+turtle.goto(0,0)
+turtle.pendown()
+turtle.goto(350,0)
+turtle.penup()
+turtle.goto(x5,y0)
+turtle.dot()
+turtle.goto(x5-7.5,10)
+turtle.write("5")
+turtle.goto(x10,y0)
+turtle.dot()
+turtle.goto(x10-7.5,10)
+turtle.write("10")
+turtle.goto(x15,y0)
+turtle.dot()
+turtle.goto(x15-7.5,10)
+turtle.write("15")
+turtle.goto(x20,y0)
+turtle.dot()
+turtle.goto(x20-7.5,10)
+turtle.write("20")
+turtle.goto(x25,y0)
+turtle.dot()
+turtle.goto(x25-7.5,10)
+turtle.write("25")
+turtle.goto(x30,y0)
+turtle.dot()
+turtle.goto(x30-7.5,10)
+turtle.write("30")
+turtle.goto(x35,y0)
+turtle.dot()
+turtle.goto(x35-7.5,10)
+turtle.write("35")
+turtle.goto(x40,y0)
+turtle.dot()
+turtle.goto(x40-7.5,10)
+turtle.write("40")
+turtle.goto(x45,y0)
+turtle.dot()
+turtle.goto(x45-7.5,10)
+turtle.write("45")
+turtle.goto(x50,y0)
+turtle.dot()
+turtle.goto(x50-7.5,10)
+turtle.write("50")
+turtle.goto(x55,y0)
+turtle.dot()
+turtle.goto(x55-7.5,10)
+turtle.write("55")
+turtle.goto(x60,y0)
+turtle.dot()
+turtle.goto(x60-7.5,10)
+turtle.write("60")
+turtle.goto(x65,y0)
+turtle.dot()
+turtle.goto(x65-7.5,10)
+turtle.write("65")
+turtle.goto(x70,y0)
+turtle.dot()
+turtle.goto(x70-7.5,10)
+turtle.write("70")
+turtle.goto(x75,y0)
+turtle.dot()
+turtle.goto(x75-7.5,10)
+turtle.write("75")
+turtle.goto(x80,y0)
+turtle.dot()
+turtle.goto(x80-7.5,10)
+turtle.write("80")
+turtle.goto(x85,y0)
+turtle.dot()
+turtle.goto(x85-7.5,10)
+turtle.write("85")
+turtle.goto(x90,y0)
+turtle.dot()
+turtle.goto(x90-7.5,10)
+turtle.write("90")
+turtle.goto(x95,y0)
+turtle.dot()
+turtle.goto(x95-7.5,10)
+turtle.write("95")
+turtle.goto(x100,y0)
+turtle.dot()
+turtle.goto(x100-7.5,10)
+turtle.write("100")
+turtle.goto(0,0)
+turtle.pendown()
+
+#start of negative x graph
+turtle.goto(-350,0)
+turtle.penup()
+turtle.goto(nx5,y0)
+turtle.dot()
+turtle.goto(nx5-7.5,10)
+turtle.write("-5")
+turtle.goto(nx10,y0)
+turtle.dot()
+turtle.goto(nx10-7.5,10)
+turtle.write("-10")
+turtle.goto(nx15,y0)
+turtle.dot()
+turtle.goto(nx15-7.5,10)
+turtle.write("-15")
+turtle.goto(nx20,y0)
+turtle.dot()
+turtle.goto(nx20-7.5,10)
+turtle.write("-20")
+turtle.goto(nx25,y0)
+turtle.dot()
+turtle.goto(nx25-7.5,10)
+turtle.write("-25")
+turtle.goto(nx30,y0)
+turtle.dot()
+turtle.goto(nx30-7.5,10)
+turtle.write("-30")
+turtle.goto(nx35,y0)
+turtle.dot()
+turtle.goto(nx35-7.5,10)
+turtle.write("-35")
+turtle.goto(nx40,y0)
+turtle.dot()
+turtle.goto(nx40-7.5,10)
+turtle.write("-40")
+turtle.goto(nx45,y0)
+turtle.dot()
+turtle.goto(nx45-7.5,10)
+turtle.write("-45")
+turtle.goto(nx50,y0)
+turtle.dot()
+turtle.goto(nx50-7.5,10)
+turtle.write("-50")
+turtle.goto(nx55,y0)
+turtle.dot()
+turtle.goto(nx55-7.5,10)
+turtle.write("-55")
+turtle.goto(nx60,y0)
+turtle.dot()
+turtle.goto(nx60-7.5,10)
+turtle.write("-60")
+turtle.goto(nx65,y0)
+turtle.dot()
+turtle.goto(nx65-7.5,10)
+turtle.write("-65")
+turtle.goto(nx70,y0)
+turtle.dot()
+turtle.goto(nx70-7.5,10)
+turtle.write("-70")
+turtle.goto(nx75,y0)
+turtle.dot()
+turtle.goto(nx75-7.5,10)
+turtle.write("-75")
+turtle.goto(nx80,y0)
+turtle.dot()
+turtle.goto(nx80-7.5,10)
+turtle.write("-80")
+turtle.goto(nx85,y0)
+turtle.dot()
+turtle.goto(nx85-7.5,10)
+turtle.write("-85")
+turtle.goto(nx90,y0)
+turtle.dot()
+turtle.goto(nx90-7.5,10)
+turtle.write("-90")
+turtle.goto(nx95,y0)
+turtle.dot()
+turtle.goto(nx95-7.5,10)
+turtle.write("-95")
+turtle.goto(nx100,y0)
+turtle.dot()
+turtle.goto(nx100-13.5,10)
+turtle.write("-100")
+turtle.pendown()
+
+correct_answer='n'
+you_shur = 'y'
+while correct_answer == 'n':
+	shape = 0
+	while shape != 1 and shape != 2 and shape != 3:
+		print('***************************')
+		print('* hit 1 for a line        *')
+		print('*hit 2 for a trianle      *')
+		print('*hit 3 for a quadrelateral*')
+		print('***************************')
+		shape=int(input('what shape do you want to draw:'))
+		if shape == 1:
+			print('you picked the opption line')
+			c=x5
+			d=y5
+			turtle.goto(c,d)
+		elif shape == 2:
+			print('you picked the opption triangle')
+		elif shape == 3 :
+			print('you picked the opption quadrelateral') 
+		correct_answer = input('is this the opption that you wanted (answer in n for no or y for yes):')
+
+
+
+while correct_answer == 'y' and you_shur == 'y':
+	you_shur = input('do you want to draw a nother shape:')
+	if you_shur == 'y':
+		correct_answer='n'
+		while correct_answer == 'n':
+			shape = 0
+			while shape != 1 and shape != 2 and shape != 3:
+				print('***************************')
+				print('* hit 1 for a line        *')
+				print('*hit 2 for a trianle      *')
+				print('*hit 3 for a quadrelateral*')
+				print('***************************')
+				shape=int(input('what shape do you want to draw:'))
+				if shape == 1:
+					print('you picked the opption line')
+				elif shape == 2:
+					print('you picked the opption triangle')
+				else :
+						print('you picked the opption quadrelateral') 
+				correct_answer = input('is this the opption that you wanted (answer in n for no or y for yes):')
+
+print('you quit the program') 
