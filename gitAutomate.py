d0c0f7f6fbfc78260536f5effe56da3943d9620b
vlad0318 <revutskyyvlad@gmail.com>
2020-12-29 13:37:52 -0800

    update

--- gitAutomate.py
+++ gitAutomate.py
@@ -3,5 +3,5 @@ import os
 os.system('git status')
 os.system('git add -A')
 os.system('git status')
-os.system('git commit -m /"update/"')
-
+os.system('git commit -m "update"')
+os.system('git push -u origin master')
